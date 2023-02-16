f6e5c1e777ad3fdaccae460419ec6d7c6548c0b0
rcocera <125590801+rcocera@users.noreply.github.com>
2023-02-16 17:49:52 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+let numero = 0
+input.onGesture(Gesture.Shake, function () {
+    numero = randint(0, 6)
+    if (numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (numero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (numero == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (numero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (numero == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
